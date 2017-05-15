cef3aba53321424b6ecec1acdd5242ad0a148565
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-05-15 11:00:13 -0600

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -176,7 +176,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(40.5853, -105.0844);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
